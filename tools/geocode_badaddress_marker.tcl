f9347b939da6f2785cdab9a10b2be5dd53aa1d23
Ru Uba <remethyst@yahoo.com>
2020-02-05 15:08:08 -0500

    comments + make ending print statement to path
    stores the path in variable.
    adds comments in case of column changes
    adds print statement on completion

--- tools/geocode_badaddress_marker.tcl
+++ tools/geocode_badaddress_marker.tcl
@@ -7,19 +7,21 @@
 
 set filename [open [lindex $argv 0] "r"]
 # TODO use environment temp variable for platform agnosticism
-set tempfile [open [string cat "/tmp/temp-" [file tail [lindex $argv 0]]] "w"]
+set path  [string cat "/tmp/temp-" [clock clicks] [file tail [lindex $argv 0]]]
+set tempfile [open $path "w"]
 set lineno 0
 
 while {[gets $filename line] >= 0} {
     set lineno [expr {$lineno + 1}]
 
-# TODO use the package require csv so that can split better conditions
     set splitted [split $line ,]
+    # col0 should be the 'EIN' column. Change accordingly.
     set col0 [lindex $splitted 0]
+    # col3 represents the 'STREET' column. Change accordingly
     set col3 [lindex $splitted 3]
 
     if {[regexp {^[^0-9]} $col3]} {
-	      puts "Match found! on $lineno ---> $col3"
+	puts "Match found! on $lineno ---> $col3"
         set col0 [string cat ZZIGNORE $col0]
         puts "Converted match to $col0"
         set splitted [lreplace $splitted 0 0 $col0]
@@ -32,4 +34,4 @@ while {[gets $filename line] >= 0} {
 
 close $filename
 close $tempfile
-
+puts "Modified file written to $path" 
