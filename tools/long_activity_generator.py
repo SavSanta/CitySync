4763f8e2ec59f096be38b3a8aa8c0af63240686b
Ru Uba <remethyst@yahoo.com>
2020-03-05 12:51:43 -0500

    backport fixes from master

--- tools/long_activity_generator.py
+++ tools/long_activity_generator.py
@@ -11,7 +11,8 @@ from sys import argv, exit, exc_info
 def process_data(row):
     ''' Strips the csv.DictReader data from tuplesets to writeable list values'''
     new_row = list(row.values())
-    new_row.append(convertcode(new_row[col_index]))
+    # Change the last column to the longform of activity names
+    new_row[-1] = convertcode(new_row[col_index])
     return new_row
 
 def convertcode(code):
@@ -335,6 +336,7 @@ else:
         try:
             col_names = reader.fieldnames
             col_index = col_names.index("ACTIVITY")
+            print("Found 'ACTIVITY' column in position {}".format(col_index))
         except ValueError:
             print("ERROR! Missing 'ACTIVITY' column header in csv!")
             exit(-2)
@@ -347,14 +349,16 @@ else:
             writer.writerow(process_data(row))
         print("Conversion completed ---> {}".format(outfilename))
 
-    except (IOError, FileNotFound) as e:
+    except (IOError, FileNotFoundError) as e:
         print("ERROR accessing file: {}".format(filename))
         raise
     except:
-        print("Unexpected ERROR!" + exc_info()[0])
+        print("Unexpected ERROR --> " + exc_info()[0].__name__)
         raise
     else:
         # Close open handles
         f.close()
         fw.close()
+
+
 
